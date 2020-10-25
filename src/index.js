997864781f6261a4b80fd054c85882fea8b2f39d
Maxim Vinnikov <maxim.vinnikoff@gmail.com>
2020-10-25 03:27:47 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 module.exports = function reverse (n) {
-  
+  const reversed = n.toString().split('').reverse();
+  return reversed[reversed.length - 1] === '-' ? reversed.slice(0, reversed.length - 1).join('') : reversed.join('');
 }
